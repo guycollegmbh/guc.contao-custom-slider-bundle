2c0d9d4916a58811fa9c934ba7e79dcbf76d0e93
Patrick Grob <p.grob@guycolle.com>
2025-06-13 14:11:35 +0200

    class name

--- src/DependencyInjection/ContaoCustomSliderExtension.php
+++ src/DependencyInjection/ContaoCustomSliderExtension.php
@@ -15,7 +15,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class ContaoCustomSliderBundle extends Extension
+class ContaoCustomSliderExtension extends Extension
 {
     /**
      * {@inheritdoc}
